2c3709cecea80c1952b6119c9cd0867784d69bea
Неприн Михаил Андреевич <neprin@sfedu.ru>
2022-12-21 16:46:49 +0300

    bat

--- production/demo_ex/run.bat
+++ production/demo_ex/run.bat
@@ -1,2 +1,2 @@
-start pip3 install .
-start demo_ex --sqlite3 .\demo_ex\data\db.sqlite3 --neat-config .\demo_ex\data\neat_config.txt
+start pip3 install -e .
+demo_ex --sqlite3 .\demo_ex\data\db.sqlite3 --neat-config .\demo_ex\data\neat_config.txt
